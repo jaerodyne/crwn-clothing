d9289ddede56b87c2d5bedb4064b4d16be818f6e
jaerodyne <jsomera@gmail.com>
2019-12-31 21:10:45 -0600

    Add cart item to dropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,8 +4,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   )
 
   if(existingCartItem) {
-    cartItems.map(cartItem =>
-      cartItem.id === cartItemToAdd
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
       ? {...cartItem, quantity: cartItem.quantity + 1 }
       : cartItem
     )
