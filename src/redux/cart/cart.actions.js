c78b12d603b1fc620a6930ffeff6e406219e5e78
jaerodyne <jsomera@gmail.com>
2020-01-02 02:32:57 -0600

    Add functionality to remove items from cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,6 +1,5 @@
 import { CartActionTypes } from './cart.types';
 
-
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
@@ -9,3 +8,8 @@ export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
 });
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+})
